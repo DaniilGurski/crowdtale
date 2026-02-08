35b9a1a345be67a48d6d96081008e0212abf83eb
Daniil Gurski <daniilgurski6@gmail.com>
2026-02-08 22:45:53 +0100

    add placeholder stories, load story card contents dynamically

--- src/components/StoryCard.tsx
+++ src/components/StoryCard.tsx
@@ -5,70 +5,58 @@ import {
   CardFooter,
   CardHeader,
   CardTitle,
-} from "./ui/card";
+} from "@components/ui/card";
 import { Badge } from "@components/ui/badge";
 import { Clock, PenIcon } from "lucide-react";
+import type { Story } from "@/types";
 
-export default function StoryCard() {
+interface StoryCardProps {
+  story: Story;
+}
+
+export default function StoryCard({ story }: StoryCardProps) {
+  const { title, genres, contents, creatorName, createdAt } = story;
+  const introText = contents[0].text;
+
+  const getDaysDiff = () => {
+    const now = Date.now();
+
+    const diffInDays = Math.floor(
+      (now - createdAt.getTime()) / (1000 * 60 * 60 * 24),
+    );
+
+    return diffInDays;
+  };
+
   return (
     <Card className="flex h-full snap-start flex-col rounded-none">
       <CardHeader>
-        <CardTitle className="text-xl"> Story Name </CardTitle>
+        <CardTitle className="text-xl"> {title} </CardTitle>
         <ul className="flex flex-wrap gap-x-2">
-          <li>
-            <Badge> Sci-Fi </Badge>
-          </li>
-          <li>
-            <Badge> Romance </Badge>
-          </li>
+          {genres.map((genre) => (
+            <li>
+              <Badge> {genre} </Badge>
+            </li>
+          ))}
         </ul>
       </CardHeader>
 
       <CardContent className="flex-1">
-        <p className="line-clamp-4 tracking-wider italic">
-          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis
-          culpa nemo nam aliquid, dolore repudiandae dolores odio deleniti et
-          eligendi doloremque itaque pariatur ex veniam qui vitae, recusandae
-          vero velit."
-        </p>
+        <p className="line-clamp-4 tracking-wider italic">"{introText}"</p>
       </CardContent>
 
       <CardFooter className="grid gap-y-4">
         <div className="flex justify-between">
           <p className="flex gap-x-2">
-            <PenIcon /> <span> By: Lorem </span>
+            <PenIcon /> <span> By: {creatorName} </span>
           </p>
           <p className="flex gap-x-2">
             <Clock />
-            <span> 42 days ago </span>
+            <span> {getDaysDiff()} days ago </span>
           </p>
         </div>
         <Button> Join Story </Button>
       </CardFooter>
     </Card>
   );
-}
-
-{
-  /* <article>
-  <header>
-    <h3 className="font-bold"> Story Name </h3>
-    <ul>
-      <li>Fantasy</li>
-      <li>Adventure</li>
-    </ul>
-  </header>
-  <div className="flex-1">
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis culpa
-    nemo nam aliquid, dolore repudiandae dolores odio deleniti et eligendi
-    doloremque itaque pariatur ex veniam qui vitae, recusandae vero velit.
-  </div>
-  <footer>
-    <div>
-      <p> By: @</p>
-      <p> @ days ago </p>
-    </div>
-    <Button> Join Story </Button>
-  </footer>
-</article>; */
 }
--- src/pages/DiscoverPage.tsx
+++ src/pages/DiscoverPage.tsx
@@ -1,12 +1,12 @@
 import StoryCard from "@/components/StoryCard";
+import { stories } from "@/stories";
 
 export default function DiscoverPage() {
   return (
     <section className="mx-auto h-screen w-[90%] max-w-3xl snap-y snap-mandatory overflow-y-scroll">
-      {/* <h2> Discover Page </h2> */}
-      <StoryCard />
-      <StoryCard />
-      <StoryCard />
+      {stories.map((story) => {
+        return <StoryCard key={story.id} story={story} />;
+      })}
     </section>
   );
 }
