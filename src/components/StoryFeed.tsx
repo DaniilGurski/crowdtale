2b3ecd5ff039f50923cb85a5a727c63f9646ec23
Daniil Gurski <daniilgurski6@gmail.com>
2026-02-12 16:11:24 +0100

    add loading spinner, implement error and skeleton card

--- src/components/StoryFeed.tsx
+++ src/components/StoryFeed.tsx
@@ -1,11 +1,22 @@
 import { useInView } from "react-intersection-observer";
 import { useEffect } from "react";
-import StoryCard from "@components/StoryCard";
+import StoryCard, { ErrorStoryCard } from "@components/StoryCard";
+import { Spinner } from "@components/ui/spinner";
+import {
+  Empty,
+  EmptyContent,
+  EmptyDescription,
+  EmptyHeader,
+  EmptyMedia,
+  EmptyTitle,
+} from "@components/ui/empty";
+import { Button } from "@components/ui/button";
 import { useDiscoveryFeed } from "@hooks/useDiscoveryFeed";
+import { Compass } from "lucide-react";
 
 export default function StoryFeed() {
   const { ref, inView } = useInView({ threshold: 0.2 });
-  const { data, isPending, error, fetchNextPage } = useDiscoveryFeed();
+  const { data, isPending, error, fetchNextPage, refetch } = useDiscoveryFeed();
   const stories = data?.pages.flat() ?? [];
 
   useEffect(() => {
@@ -13,15 +24,15 @@ export default function StoryFeed() {
   }, [inView, fetchNextPage]);
 
   if (isPending) {
-    return <p> Loading stories ... </p>;
+    return <Spinner className="size-6" />;
   }
 
   if (error) {
-    return <p> Error: {error.message} </p>;
+    return <ErrorStoryCard refetch={refetch} />;
   }
 
   if (!stories) {
-    return <p> No stories yet ! </p>;
+    return <EmptyFeed />;
   }
 
   return (
@@ -38,5 +49,27 @@ export default function StoryFeed() {
         );
       })}
     </>
+  );
+}
+
+// TODO: Move to seperate file
+function EmptyFeed() {
+  return (
+    <Empty>
+      <EmptyHeader>
+        <EmptyMedia variant="icon">
+          <Compass />
+        </EmptyMedia>
+        <EmptyTitle> No stories found ! </EmptyTitle>
+
+        <EmptyDescription>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam quidem
+          natus, magnam esse molestias maiores perferendis.
+        </EmptyDescription>
+      </EmptyHeader>
+      <EmptyContent>
+        <Button> Create story </Button>
+      </EmptyContent>
+    </Empty>
   );
 }
